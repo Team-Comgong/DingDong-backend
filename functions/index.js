f0f25e7220c765c21bebc05a788c6089cf43ed97
BSAA7567 <33346331+BSAA7567@users.noreply.github.com>
2020-06-04 16:43:36 +0900

    크롤링 배열 DB 처리
    날짜 스탬프 추가
    
    파싱 작업 진행중

--- functions/index.js
+++ functions/index.js
@@ -11,27 +11,50 @@ admin.initializeApp();
 exports.helloWorld = functions
     .region('asia-northeast1')
     .https
-    .onRequest(async(request, response) => {
-        const arr = ['http://ncov.mohw.go.kr', 'https://m.stock.naver.com', 'https://m.stock.naver.com/marketindex/index.nhn?menu=exchange#exchange'];
+    .onRequest(async (request, response) => {
+        const arr = [
+            'http://ncov.mohw.go.kr', 'https://m.stock.naver.com', 'https://m.stock.naver.com/marketindex/item.nhn?marketindexCd=FX_USDKRW&menu=ex' +
+                    'change'
+        ];
         const result = new Array();
         for (let i = 0; i < arr.length; i++) {
-            await axios.get(arr[i]).then(html=>{
-                const $ = cheerio.load(html.data);
-                switch (i) {
-                    case 0:
-                        result.push($('body > div > div.mainlive_container > div.container > div > div.liveboard_layout > div.liveNumOuter > ul > li:nth-child(1) > span.data1').text());
-                        break;
-                    case 1:
-                        result.push($('#mflick > div > div.flick-ct.dmst._tab._index_wrapper._polling > div > ul').text().replace(/^\s+|\t/g,''));
-                        break;
-                    case 2:
-                        result.push($('#content > div > div.ct_box.intnl_major_item > ul > li:nth-child(1) > a > div.price_wrp').text().replace(/\t/g,''));
-                        break;
-                    default:
-                        console.log('index error');
-                        break;
-                }
-            })
-            console.log(result);
+            await axios
+                .get(arr[i])
+                .then(html => {
+                    const $ = cheerio.load(html.data);
+                    switch (i) {
+                        case 0:
+                            result.push('코로나 일일 확진자 : ' + $(
+                                'body > div > div.mainlive_container > div.container > div > div.liveboard_layo' +
+                                'ut > div.liveNumOuter > ul > li:nth-child(1) > span.data1'
+                            ).text());
+                            break;
+                        case 1:
+                            result.push(
+                                $('#mflick > div > div.flick-ct.dmst._tab._index_wrapper._polling > div > ul').text().replace(/^\s+|\t/g, '')
+                            );
+                            break;
+                        case 2:
+                            result.push('오늘의 달러 환율 : ' + $(
+                                '#header > div.major_info_wrp.no_chart.no_code > div.major_info > div.stock_wrp' +
+                                ' > div'
+                            ).text().replace(/^\s+|\t/g, ''));
+                            break;
+                        default:
+                            console.log('index error');
+                            break;
+                    }
+                })
+
         }
+        const today = new Date();
+        const year = today.getFullYear();
+        const month = today.getMonth() + 1;
+        const date = today.getDate();
+        const name = year + '-' + month + '-' + date
+        console.log('Today is : ', name);
+        admin
+            .database()
+            .ref(name)
+            .set({result});
     });
