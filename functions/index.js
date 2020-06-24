9367103b9f9cb57c2d0574e29652877c07d8ce37
BSAA7567 <33346331+BSAA7567@users.noreply.github.com>
2020-06-24 15:38:46 +0900

    크롤링 & FCM 날짜 수정
    이젠 당일로

--- functions/index.js
+++ functions/index.js
@@ -96,7 +96,7 @@ exports.sendFCM = functions
         const today = new Date();
         const year = today.getFullYear();
         const month = today.getMonth() + 1;
-        const date = today.getDate() - 1;
+        const date = today.getDate();
 
         const title = year + '-' + month + '-' + date;
         const context = await admin
